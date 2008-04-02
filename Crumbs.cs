manicolosi/manico-crumbs

commit f37f17c5de2d4efe5cde248ecf6885a4aa3d072b

Hover bug fixed
Occasionally a crumb would get stuck as being hovered event after the
mouse cursor has left the window. This is because Crumbs depended on
motion notify events to un-hover a crumb. If the cursor was flicked
across the crumb, it would get the notification and set the proper crumb
to hover, but when the mouse cursor left it would get stuck there,
because there wasn't a notification that the cursor left.

Now we use leave notification to ensure when the cursor is out of the
widget there is no hovered crumb.

Crumbs.cs
@@ -361,6 +361,7 @@ namespace Egg
 				| Gdk.EventMask.ExposureMask
 				| Gdk.EventMask.ScrollMask
 				| Gdk.EventMask.PointerMotionMask
+                | Gdk.EventMask.LeaveNotifyMask
 				| Gdk.EventMask.ButtonPressMask
 				| Gdk.EventMask.ButtonReleaseMask
 				| Gdk.EventMask.KeyPressMask
@@ -514,6 +515,14 @@ namespace Egg
 
 			return base.OnMotionNotifyEvent (evnt);
 		}
+
+        protected override bool OnLeaveNotifyEvent (EventCrossing evnt)
+        {
+            this.m_Hover = null;
+            this.QueueDraw ();
+
+            return base.OnLeaveNotifyEvent (evnt);
+        }
 
 		protected override bool OnButtonReleaseEvent (EventButton evnt)
 		{

commit 20661f9077a6d63272e831c788bbf3322629d3dc

Fixed click selection bug
Commit f37f17c5 introduced a bug that made it impossible to select a
crumb by clicking. When the user would click a crumb,
OnLeaveNotifyEvent() is called which "un-hovers" the crumb. Then
OnButtonReleaseEvent() is called but there is no "hovered" crumb to set
as active.

Crumbs.cs
@@ -445,8 +445,10 @@ namespace Manico
 
         protected override bool OnLeaveNotifyEvent (EventCrossing evnt)
         {
-            this.m_Hover = null;
-            this.QueueDraw ();
+            if (evnt.Mode == CrossingMode.Normal) {
+                this.m_Hover = null;
+                this.QueueDraw ();
+            }
 
             return base.OnLeaveNotifyEvent (evnt);
         }